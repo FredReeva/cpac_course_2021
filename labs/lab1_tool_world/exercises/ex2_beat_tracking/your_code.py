1e5904dad47820abbb5f1df9da14490e0a599e63
Federico Maver <maver.federico@gmail.com>
2021-10-06 13:57:35 +0200

    ex2-beat tracking complete

--- labs/lab1_tool_world/exercises/ex2_beat_tracking/your_code.py
+++ labs/lab1_tool_world/exercises/ex2_beat_tracking/your_code.py
@@ -1,11 +1,16 @@
 # any import? 
+import librosa
 
 def compute_beats(y, sr):
     # your code here
-    return 0
+    tempo, beats = librosa.beat.beat_track(y, sr, units='samples')
+    return beats
+
 def add_samples(y, sample, beats):
     y_out=y.copy()
     # your code here ...
-
+    for i in beats:
+        y_out[i:i+len(sample)] += sample
+    
     return y_out
 
